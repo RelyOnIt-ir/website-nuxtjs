304da971512028221c4fc732d630f72ca3c67be8
Sadegh Mohebbi <mohebbi.sadegh@gmail.com>
2021-05-05 13:45:52 +0430

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: 'ریلیونیت | RelyOnIt',
+    title: 'ریلایونیت | RelyOnIt',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
